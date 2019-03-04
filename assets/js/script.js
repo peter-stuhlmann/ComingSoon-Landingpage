f37d887385b497050d4d67a2790609846c75da2e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-04 23:33:02 +0100

    Removed jquery code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function countDown() {
-    
-    let $clock = $('#countdown'),
+
+    let clock = document.querySelector('#countdown'),
         countdownEnd = moment('01-05-2019 09:00:00', 'DD-MM-YYYY HH:mm:ss').unix(),
         currentTime = moment().unix(),
         timeDifference = countdownEnd - currentTime,
@@ -8,11 +8,6 @@ function countDown() {
         interval = 1000;
 
     if (timeDifference > 0) {
-
-        let $days = $('#days').appendTo($clock),
-            $hours = $('#hours').appendTo($clock),
-            $minutes = $('#minutes').appendTo($clock),
-            $seconds = $('#seconds').appendTo($clock);
 
         setInterval(function () {
 
@@ -22,15 +17,10 @@ function countDown() {
                 minutes = `<span>minutes</span><br>` + moment.duration(duration).minutes(),
                 seconds = `<span>seconds</span><br>` + moment.duration(duration).seconds();
 
-            days = $.trim(days).length === 1 ? '0' + days : days;
-            hours = $.trim(hours).length === 1 ? '0' + hours : hours;
-            minutes = $.trim(minutes).length === 1 ? '0' + minutes : minutes;
-            seconds = $.trim(seconds).length === 1 ? '0' + seconds : seconds;
-
-            $days.html(days);
-            $hours.html(hours);
-            $minutes.html(minutes);
-            $seconds.html(seconds);
+            document.querySelector('#days').innerHTML = days
+            document.querySelector('#hours').innerHTML = hours
+            document.querySelector('#minutes').innerHTML = minutes
+            document.querySelector('#seconds').innerHTML = seconds
 
         }, interval);
     }
