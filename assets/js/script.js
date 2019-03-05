68c7c9a105f6bd79af9cca48409b5f4039f09fe1
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-05 15:42:32 +0100

    Fixed a bug: now it works also when the countdownEnd is longer than 31 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,15 @@ function countDown() {
         timeDifference = countdownEnd - currentTime,
         duration = moment.duration(timeDifference * 1000, 'milliseconds'),
         interval = 1000;
+    
+    console.log(timeDifference)
 
     if (timeDifference > 0) {
 
         setInterval(function () {
 
             duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
-            let days = `<span>days</span><br>` + moment.duration(duration).days(),
+            let days = `<span>days</span><br>` + Math.round(timeDifference / 86400),
                 hours = `<span>hours</span><br>` + moment.duration(duration).hours(),
                 minutes = `<span>minutes</span><br>` + moment.duration(duration).minutes(),
                 seconds = `<span>seconds</span><br>` + moment.duration(duration).seconds();
