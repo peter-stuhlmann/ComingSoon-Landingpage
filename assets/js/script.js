1086d84ae4b49aba8b4dbf07b90d75e66f5ec59d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-06 21:59:58 +0100

    Moved the output strings to DOM section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let countdownEnd = moment('06-03-2019 21:50:08', 'DD-MM-YYYY HH:mm:ss').unix(),
+let countdownEnd = moment('06-03-2019 21:53:08', 'DD-MM-YYYY HH:mm:ss').unix(),
     currentTime = moment().unix(),
     duration = moment.duration((countdownEnd - currentTime - 1) * 1000, 'milliseconds'),
     interval = 1000;
@@ -6,15 +6,15 @@ let countdownEnd = moment('06-03-2019 21:50:08', 'DD-MM-YYYY HH:mm:ss').unix(),
 let countDown = setInterval(function () {
 
     duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
-    let days = `<span>days</span><br>` + Math.floor(duration / 1000 / 86400),
-        hours = `<span>hours</span><br>` + moment.duration(duration).hours(),
-        minutes = `<span>minutes</span><br>` + moment.duration(duration).minutes(),
-        seconds = `<span>seconds</span><br>` + moment.duration(duration).seconds();
+    let days =  Math.floor(duration / 1000 / 86400),
+        hours = moment.duration(duration).hours(),
+        minutes = moment.duration(duration).minutes(),
+        seconds = moment.duration(duration).seconds();
 
-    document.querySelector('#days').innerHTML = days
-    document.querySelector('#hours').innerHTML = hours
-    document.querySelector('#minutes').innerHTML = minutes
-    document.querySelector('#seconds').innerHTML = seconds
+    document.querySelector('#days').innerHTML = `<span>days</span><br>` + days
+    document.querySelector('#hours').innerHTML = `<span>hours</span><br>` + hours
+    document.querySelector('#minutes').innerHTML = `<span>minutes</span><br>` + minutes
+    document.querySelector('#seconds').innerHTML = `<span>seconds</span><br>` + seconds
 
     if (duration.asMilliseconds() <= 0) {
         clearInterval(countDown)
