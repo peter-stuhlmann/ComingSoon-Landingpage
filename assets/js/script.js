8d5cf523d77a22e753be2151abb115c1c8fba258
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-27 23:42:59 +0100

    Moved position of the units

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let countdownEnd = moment('06-03-2019 21:53:08', 'DD-MM-YYYY HH:mm:ss').unix(),
+let countdownEnd = moment('16-03-2019 21:53:08', 'DD-MM-YYYY HH:mm:ss').unix(),
     currentTime = moment().unix(),
     duration = moment.duration((countdownEnd - currentTime - 1) * 1000, 'milliseconds'),
     interval = 1000;
@@ -11,16 +11,16 @@ let countDown = setInterval(function () {
         minutes = moment.duration(duration).minutes(),
         seconds = moment.duration(duration).seconds();
 
-    document.querySelector('#days').innerHTML = `<span>days</span><br>` + days
-    document.querySelector('#hours').innerHTML = `<span>hours</span><br>` + hours
-    document.querySelector('#minutes').innerHTML = `<span>minutes</span><br>` + minutes
-    document.querySelector('#seconds').innerHTML = `<span>seconds</span><br>` + seconds
+    document.querySelector('#days').innerHTML = `<span>${days}</span><br><span>days</span>`
+    document.querySelector('#hours').innerHTML = `<span>${hours}</span><br><span>hours</span>`
+    document.querySelector('#minutes').innerHTML = `<span>${minutes}</span><br><span>minutes</span>`
+    document.querySelector('#seconds').innerHTML = `<span>${seconds}</span><br><span>seconds</span>`
 
     if (duration.asMilliseconds() <= 0) {
         clearInterval(countDown)
-        document.querySelector('#days').innerHTML = `<span>days</span><br>0`
-        document.querySelector('#hours').innerHTML = `<span>hours</span><br>0`
-        document.querySelector('#minutes').innerHTML = `<span>minutes</span><br>0`
-        document.querySelector('#seconds').innerHTML = `<span>seconds</span><br>0`
+        document.querySelector('#days').innerHTML = `<span>0</span><br><span>days</span>`
+        document.querySelector('#hours').innerHTML = `<span>0</span><br><span>hours</span>`
+        document.querySelector('#minutes').innerHTML = `<span>0</span><br><span>minutes</span>`
+        document.querySelector('#seconds').innerHTML = `<span>0</span><br><span>seconds</span>`
     }
 }, interval);
